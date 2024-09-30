4d4c6ee315519fc88ed1cbeadcfe9ee91d31058c
Dan Klco <klco@adobe.com>
2024-09-29 22:08:56 -0400

    fix: redirects

--- packages/frontend/astro.config.mjs
+++ packages/frontend/astro.config.mjs
@@ -3,7 +3,6 @@ import mdx from "@astrojs/mdx";
 import pagefind from "astro-pagefind";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
-import redirects from "./config/redirects.mjs";
 
 export default defineConfig({
   site: "https://danklco.com",
@@ -16,6 +15,17 @@ export default defineConfig({
       SVG: false,
     }),
   ],
-  redirects,
   output: "static",
+  markdown: {
+    shikiConfig: {
+      theme: "github-dark-high-contrast",
+      transformers: [
+        {
+          pre: (node) => {
+            delete node.properties.tabindex;
+          },
+        },
+      ],
+    },
+  },
 });
