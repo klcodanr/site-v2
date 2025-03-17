636a763507be2c6943dfc465186c3387efec94f6
Dan Klco <klco@adobe.com>
2025-03-17 05:45:59 -0400

    Adding more tracing on failure and fixing app name

--- sst.config.ts
+++ sst.config.ts
@@ -3,7 +3,7 @@
 export default $config({
   app(input) {
     return {
-      name: "thecaringplace-info",
+      name: "danklco-com",
       home: "aws",
       removal: input?.stage === "production" ? "retain" : "remove",
       providers: {
